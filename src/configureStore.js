406e004a79ed748e44eeda9391adae4902245448
opelar <godmingliang@qq.com>
2018-04-01 21:34:29 +0800

    cfg

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "./reducer/rootReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 // import { fromJS, is } from "immutable";
 
 function configureStore() {
   let store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(thunkMiddleware))
+    applyMiddleware(thunkMiddleware)
   );
 
   if (window) {
